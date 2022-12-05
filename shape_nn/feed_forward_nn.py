948991caddf79e3c2f8e97e099ff7d587c5ebc1b
nualapennycook <nap40@bath.ac.uk>
2022-12-05 10:44:57 +0000

    Messing around with initial conditions

--- shape_nn/feed_forward_nn.py
+++ shape_nn/feed_forward_nn.py
@@ -16,18 +16,22 @@ class Feedforward(torch.nn.Module):
         self.input_size = input_size
         self.hidden_size  = hidden_size
         self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.w = torch.empty(self.input_size, self.hidden_size)
+        torch.nn.init.eye_(self.w)
         self.fc15 = torch.nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc15.weight.data.copy_(torch.eye(self.hidden_size))
         self.tanh = torch.nn.Tanh()
         self.relu = torch.nn.ReLU()
         # Defining the output size of the output 
-        self.fc2 = torch.nn.Linear(self.hidden_size, 2)
+        self.fc2 = torch.nn.Linear(self.hidden_size, self.input_size)
         self.sigmoid = torch.nn.Sigmoid()
              
     def forward(self, x):
+        # hidden1 = x@self.w
         hidden1 = self.fc1(x)
         hidden2 = self.fc15(hidden1)
-        hidden3 = self.fc15(hidden2)
-        relu = self.tanh(hidden3)
-        output = self.fc2(relu)
+        # hidden3 = self.fc15(hidden2)
+        tahn = self.tanh(hidden2)
+        output = self.fc2(tahn)
         # output = self.sigmoid(output)
         return output
