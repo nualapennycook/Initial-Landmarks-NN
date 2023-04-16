a20a5ff28f9ad9dc607e9a96deab8d0308bed469
nualapennycook <nap40@bath.ac.uk>
2023-04-16 10:09:40 +0100

    Added further comments

--- cat_to_plane_anim.py
+++ cat_to_plane_anim.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 
 '''
-Demo to animate the cat to plane registration.
+Animation showing the approximation to image registration at each step in the neural network.
+The template shape is a cat. The target shape is a plane.
 '''
 
 epoch = 1000
@@ -13,12 +14,12 @@ epoch = 1000
 def main():
     # Firstly need to extract the training data from the text files
     # We train the network for a set shape to another set shape
-    input_data = RegisterShapeData(path_to_shape_data='shape_landmark_data/train_data/cat.txt')
+    input_data = RegisterShapeData(path_to_shape_data='shape_landmark_data/cat.txt')
     input_data.extract_shape_data()
     input_data.centre_shape()
     input_data.scale_shape()
 
-    set_data = RegisterShapeData(path_to_shape_data='shape_landmark_data/train_data/plane.txt')
+    set_data = RegisterShapeData(path_to_shape_data='shape_landmark_data/plane.txt')
     set_data.extract_shape_data()
     set_data.centre_shape()
     set_data.scale_shape()
@@ -26,11 +27,11 @@ def main():
     # Rotating the input data to the orientation of the set data
     x_data = rotate_shape(rotate_data=input_data.shape_data, fixed_data=set_data.shape_data)
 
-    # Reorgainising/reshaping the data into pairs
+    # Reshaping the data into coordinate pairs for input to the neural network
     x_data = [[x_data[0][i], x_data[1][i]] for i in range(len(x_data[0]))]
     y_data = [[1.1*set_data.shape_data[0][i], 1.1*set_data.shape_data[1][i]] for i in range(len(set_data.shape_data[0]))]
 
-    # Reducing the number of landmarks
+    # Reducing the number of landmarks to reduce the problem size
     skip_step = 10
     reduction = int(len(x_data)/skip_step)
     x_data = [x_data[i*skip_step] for i in range(reduction)]
@@ -39,6 +40,7 @@ def main():
     # Training the neural network
     training_landmarks, warped_x_data, y_test_pred, [train_loss, test_loss] = train_network(x_data=x_data, y_data=y_data, epoch=epoch)
 
+    # Reshaping the output data back into separate list of x and y coordinates for plotting
     plotting_x_data = [[x_data[i][0] for i in range(reduction)], [x_data[i][1] for i in range(reduction)]]
     plotting_y_data = [[y_data[i][0] for i in range(reduction)], [y_data[i][1] for i in range(reduction)]]
 
@@ -48,6 +50,7 @@ def main():
     line, = ax.plot(plotting_x_data[0], plotting_x_data[1])
     ax.legend(['initial shape', 'target shape', 'result of NN'])
 
+    # Animation to plot the image registration model for each epoch of the neural network
     def animate_shape(i):
         line.set_data(warped_x_data[i][0], warped_x_data[i][1])
         return line,
